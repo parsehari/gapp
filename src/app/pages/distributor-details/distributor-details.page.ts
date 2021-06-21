b2fbc3b5ee9c4008d9dab447a89741a5a5c39a8a
santosh <kr.santoshsinha@gmail.com>
2021-06-21 12:37:35 +0530

    updated code added

---
+++ src/app/pages/distributor-details/distributor-details.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distributor-details',
+  templateUrl: './distributor-details.page.html',
+  styleUrls: ['./distributor-details.page.scss'],
+})
+export class DistributorDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  goBack(){
+    
+  }
+}
---
+++ src/app/pages/product-list/product-list.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.page.html',
+  styleUrls: ['./product-list.page.scss'],
+})
+export class ProductListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  searchInputValueChange(event){
+  console.log("change",event);
+  }
+}
