f740e6a928e9a943e489bad17c6bb140238742aa
Hari <hari.hareshp@mnk.com>
2021-06-23 23:18:21 +0530

    menu updated

--- src/app/pages/order-details/order-details.module.ts
+++ src/app/pages/order-details/order-details.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { OrderDetailsPageRoutingModule } from './order-details-routing.module';
 
 import { OrderDetailsPage } from './order-details.page';
+import { SharedModule } from 'src/app/components/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     OrderDetailsPageRoutingModule
   ],
   declarations: [OrderDetailsPage]
