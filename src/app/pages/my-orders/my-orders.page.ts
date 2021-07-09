5ac7dccc6176b9c67b8bcf1d8e1ab144b3c890d2
santosh kumar <kr.santoshsinha@gmail.com>
2021-07-09 10:53:58 +0530

    my order api fix

--- src/app/pages/my-orders/my-orders.page.ts
+++ src/app/pages/my-orders/my-orders.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 import { ApiService } from 'src/app/services/api.service';
 import { CommonService } from 'src/app/services/common.service';
 
@@ -22,8 +23,13 @@ export class MyOrdersPage implements OnInit {
   }
 
   getMyOrders() {
+   var startdate = moment().format("DD-MM-YYYY");
+   var enddate = moment().subtract(6, "months").format('DD-MM-YYYY');
+
+    console.log("start date :",startdate);
+    console.log("end date :",enddate);
     this.commonService.showLoader();
-    this.apiService.getDataService(this.apiService.myOrders).subscribe((response: any) => {
+    this.apiService.postDataService(this.apiService.myOrders,{"OrderStartDate":'',"OrderEndDate":''}).subscribe((response: any) => {
       this.commonService.hideLoader();
       this.myOrders = response.gsk_Ord_Header_BO_List;
       console.log("my orders ", this.myOrders);
